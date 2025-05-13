8b22f5ac39e4efb021af6347522b6033372a9bed
Timothee Nolent <tnolent@f6r7s12.clusters.42paris.fr>
2025-05-13 15:47:18 +0200

    commit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tnolent <tnolent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 13:25:47 by lde-guil          #+#    #+#             */
-/*   Updated: 2025/05/12 16:01:34 by tnolent          ###   ########.fr       */
+/*   Updated: 2025/05/13 12:57:16 by tnolent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	minishell(t_exec *exec);
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	t_exec *exec;
+	t_exec	*exec;
 
 	(void)argc;
 	(void)argv;
@@ -30,19 +30,22 @@ void	minishell(t_exec *exec)
 {
 	char	*input;
 	t_list	*liste;
+	int		good;
 
+	good = 0;
 	while (1)
 	{
 		input = readline("$> ");
 		if (input == NULL)
-            break;
+			break ;
 		if (input[0] != '\0')
 		{
 			liste = initialisation();
-			if (!parsing(input, liste))
-				break;
+			if (!(good = parsing(input, liste)))
+				break ;
 			afficherliste(liste);
-			exec_line(exec, liste);
+			if (good == 1)
+				exec_line(exec, liste);
 			destruction(liste);
 		}
 	}
